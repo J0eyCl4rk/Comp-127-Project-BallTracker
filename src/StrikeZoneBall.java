d50d713682fe7117a267151c6c432e6860f00da8
J0eyCl4rk <jclark3@macalester.edu>
2020-05-01 09:37:03 -0700

    Add comments

--- src/StrikeZoneBall.java
+++ src/StrikeZoneBall.java
@@ -2,6 +2,11 @@ import comp127graphics.CanvasWindow;
 
 public class StrikeZoneBall extends Baseball {
 
+    /** Creates a Baseball to be added to the strike zone portion of the
+     * canvas. This class is a child class of Baseball.
+     * @param xPos x position of the ball
+     * @param yPos y position of the ball
+     */
     public StrikeZoneBall(double xPos, double yPos) {
         super(xPos, yPos, 0, 0);
     }
